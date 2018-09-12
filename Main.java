3791f8bf8eb3f7d2f7c3c9a681901fd10a250f81
frielliam2019 <42677906+frielliam2019@users.noreply.github.com>
2018-09-12 08:01:16 -0700

    Add files via upload
    One line code printing the l(a poem by E.E. Cummings

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("l(a\n\n\n\nle\n\naf\n\nfa\n\n\n\nll\n\n\n\ns)\n\none\n\nl\n\n\n\niness\n");
+
+    }
+}
