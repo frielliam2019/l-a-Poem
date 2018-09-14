c1346cf04331d84104ee9ca63f36643c8945ca75
frielliam2019 <42677906+frielliam2019@users.noreply.github.com>
2018-09-13 20:25:58 -0700

    Main.java
    I changed the overall spacing in between each line. I'm not entirely sure if I did this pull request correct/change correctly.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("l(a\n\n\n\nle\n\naf\n\nfa\n\n\n\nll\n\n\n\ns)\n\none\n\nl\n\n\n\niness\n");
+        System.out.println("l(a\n\nle\naf\nfa\n\nll\n\ns)\none\nl\n\niness");
 
     }
 }
